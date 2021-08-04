3955010f1a0cc9411f2023f34bbcc8a938ca0d73
Andrey Puzanov <a.puzanov@codedrop.cc>
2021-08-05 01:28:13 +0600

    Added Api for Task

---
+++ src/app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function get()
+    {
+
+    }
+
+    public function setStatus()
+    {
+
+    }
+
+    public function change()
+    {
+
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/task', [TaskController::class, 'get']);
+Route::put('/task/status', [TaskController::class, 'setStatus']);
+Route::get('/task/change', [TaskController::class, 'change']);
